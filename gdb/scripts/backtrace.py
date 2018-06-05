fb131bc05e5e9710a4595fc4ab2927c65d8377cf
sheng <sheng9571@gmail.com>
2018-06-05 15:30:30 +0800

    replace space to tab

--- gdb/scripts/backtrace.py
+++ gdb/scripts/backtrace.py
@@ -1,46 +1,46 @@
 import gdb
 
 class ShorternBacktraceCommand(gdb.Command):
-    '''Show a backtrace without argument info in each frame.'''
+	'''Show a backtrace without argument info in each frame.'''
 
-    def __init__(self):
-        super(ShorternBacktraceCommand, self).__init__ ("bt",
-                                                        gdb.COMMAND_SUPPORT,
-                                                        gdb.COMPLETE_NONE)
-    def invoke(self, arg, from_tty):
-        num = 0;
-        try:
-            num = int(arg)
-        except Exception as e:
-            pass
+	def __init__(self):
+		super(ShorternBacktraceCommand, self).__init__ ("bt",
+														gdb.COMMAND_SUPPORT,
+														gdb.COMPLETE_NONE)
+	def invoke(self, arg, from_tty):
+		num = 0;
+		try:
+			num = int(arg)
+		except Exception as e:
+			pass
 
-        lines = []
-        f = gdb.newest_frame()
-        fn = 0
-        while f is not None:
-            symtab_and_line = gdb.Frame.find_sal(f)
-            frame_name = gdb.Frame.name(f)
-            if frame_name:
-                args = [
-                    fn,
-                    frame_name,
-                    symtab_and_line.symtab.filename,
-                    symtab_and_line.line,
-                ]
-            else:
-                args = [fn, '??', 'unknown', 0]
-            lines.append('#%2d  %s at %s:%s' % tuple(args))
+		lines = []
+		f = gdb.newest_frame()
+		fn = 0
+		while f is not None:
+			symtab_and_line = gdb.Frame.find_sal(f)
+			frame_name = gdb.Frame.name(f)
+			if frame_name:
+				args = [
+					fn,
+					frame_name,
+					symtab_and_line.symtab.filename,
+					symtab_and_line.line,
+				]
+			else:
+				args = [fn, '??', 'unknown', 0]
+			lines.append('#%2d  %s at %s:%s' % tuple(args))
 
-            f = gdb.Frame.older(f)
-            fn += 1
+			f = gdb.Frame.older(f)
+			fn += 1
 
-        if num > 0:
-            lines = lines[:num]
-        elif num < 0:
-            lines = lines[len(lines) + num:]
+		if num > 0:
+			lines = lines[:num]
+		elif num < 0:
+		    lines = lines[len(lines) + num:]
 
-        for line in lines:
-            print (line)
+		for line in lines:
+			print (line)
 
 
 ShorternBacktraceCommand()
